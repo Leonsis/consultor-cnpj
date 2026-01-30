54019b6fc2b07cd0aa38fea5c4cad31d64b21db3
Caio Leonni <caiolenni@gmail.com>
2026-01-30 09:50:55 -0300

    Criação do backend do CNPJ

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,18 @@
 var mainInput = document.getElementById('mainInput');
-//console.log(mainInput);
-        mainInput.addEventListener('input', function (e) {
-            
-            var valor = e.target.value.replace(/\D/g, '');
 
-            if (valor.length > 14) {
-                valor = valor.slice(0, 14);
-            }        
+mainInput.addEventListener('input', function (e) {
+    
+    var valor = e.target.value.replace(/\D/g, '');
 
-            if (valor.length > 11) {               
-                valor = valor.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2}).*/, "$1.$2.$3/$4-$5");
-            } else { 
-                valor = valor.replace(/^(\d{3})(\d{3})(\d{3})(\d{2}).*/, "$1.$2.$3-$4");
-            }
-            e.target.value = valor;
-        });
+    if (valor.length > 14) {
+        valor = valor.slice(0, 14);
+    }        
+
+    if (valor.length > 11) {               
+        valor = valor.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2}).*/, "$1.$2.$3/$4-$5");
+    } else { 
+        valor = valor.replace(/^(\d{3})(\d{3})(\d{3})(\d{2}).*/, "$1.$2.$3-$4");
+    }
+    e.target.value = valor;
+});
+
